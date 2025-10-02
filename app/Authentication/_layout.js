37c39a35c3a32da421f326ecc3490f1baa705e91
Sampurna Adhikari <sampurnaa19@gmail.com>
2025-10-02 13:21:00 +1000

    renamed all tsx to js as we are using javascript and just restructuring the files

--- app/Authentication/_layout.js
+++ app/Authentication/_layout.js
@@ -1,17 +1,10 @@
-// app/Authentication/_layout.js
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
   return (
     <Stack>
-      <Stack.Screen
-        name="Login"
-        options={{ title: "Sign In", headerShown: true }}
-      />
-      <Stack.Screen
-        name="Register"
-        options={{ title: "Create Account", headerShown: true }}
-      />
+      <Stack.Screen name="Login" options={{ title: "Sign In" }} />
+      <Stack.Screen name="Register" options={{ title: "Register" }} />
     </Stack>
   );
 }
